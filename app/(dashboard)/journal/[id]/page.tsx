a144de04ce4a7655d47f9ab5a70c521025fc921d
demah <hamedsedighi@gmail.com>
2023-07-12 16:34:00 +0200

    add journal analysis

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -11,6 +11,9 @@ const getEntry = async (id: string) => {
         id,
       },
     },
+    include: {
+      analysis: true,
+    },
   })
 
   return entry
@@ -20,9 +23,32 @@ const JournalEditorPage = async ({ params }: { params: { id: string } }) => {
   const entry = await getEntry(params.id)
 
   return (
-    <div className="w-full h-full">
+    <div className="h-full w-full ">
       <Editor entry={entry} />
     </div>
+    // <div className="h-full w-full grid grid-cols-3">
+    //   <div className="col-span-2">
+    //     <Editor entry={entry} />
+    //   </div>
+    //   <div className="border-l border-black/10">
+    //     <div className="px-6 py-10" style={{ backgroundColor: color }}>
+    //       <h2 className="text-2xl">Analysis</h2>
+    //     </div>
+    //     <div>
+    //       <ul>
+    //         {analysisData.map((item) => (
+    //           <li
+    //             key={item.name}
+    //             className="px-2 py-4 flex items-ceter justify-between border-b border-t border-black/10"
+    //           >
+    //             <span className="text-lg font-semibold">{item.name}</span>
+    //             <span>{item.value}</span>
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,6 @@
 import EntryCard from '@/components/EntryCard'
 import NewEntryCard from '@/components/NewEntryCard'
+import { analyze } from '@/utils/ai'
 import { getUserFromClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import Link from 'next/link'
@@ -13,9 +14,9 @@ const getEntries = async () => {
     orderBy: {
       createdAt: 'desc',
     },
-    include: {
-      analysis: true,
-    },
+    // include: {
+    //   analysis: true,
+    // },
   })
 
   return data
